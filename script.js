5a357a818bdc1c4aa0128b8c8fdd3880760920c1
ar871 <arehman871@hotmail.com>
2023-01-17 13:43:39 +0000

    assigned all variables = lowercase/uppercase/number/special characters

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 /*password to be between 10-64 characters
 passsword to include options of having 
-lowercase
-upercase
-number
-specail characters
+create variables for 
+lowercase(done)
+upercase(done)
+number(done)
+specail characters(done)
 one input should be accepted or code should prompt user to input on 
 prompts to tell user what they want and that prompt should be saved.
 */
+console.clear();// clears the function on every refresh.
+var lowerCaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var upperCaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var numberCharacter = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var specialCharacter = ['@', '%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{', ']','[','~','-','_','.']
+  var chosenCharacter = []
+  // all variables have been assigned.
