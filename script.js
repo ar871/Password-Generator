9a8b2a32b8b1e4766ec8126d1e3e327c3020f7a9
ar871 <arehman871@hotmail.com>
2023-01-17 13:26:13 +0000

    added pseudo code to js file

---
+++ script.js
@@ -0,0 +1,9 @@
+/*password to be between 10-64 characters
+passsword to include options of having 
+lowercase
+upercase
+number
+specail characters
+one input should be accepted or code should prompt user to input on 
+prompts to tell user what they want and that prompt should be saved.
+*/
